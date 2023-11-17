68c9c4b0ac3ac2dfa75ede5dc62ea32df8265c0c
alejandromontesh <33140014+alejandromontesh@users.noreply.github.com>
2023-11-16 16:51:05 -0800

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,21 @@
+const linkedin = document.getElementById('linkedin');
+const mail = document.getElementById('mail');
+const whatsapp = document.getElementById('whatsapp');
+
+linkedin.addEventListener('click', f => {
+  window.open('https://www.linkedin.com/in/alejandromh94/', '_blank');
+});
+
+mail.addEventListener('click', f => {
+  const email = 'alejandromontesh@gmail.com';
+  const mailtoLink = `mailto:${email}`;
+  
+  window.location.href = mailtoLink;
+});
+
+whatsapp.addEventListener('click', function() {
+  const phoneNumber = '6642569336';
+  const whatsappLink = `https://wa.me/${phoneNumber}`;
+  
+  window.open(whatsappLink, '_blank');
+});
